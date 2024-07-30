ba27dfa7ceb2d964ba6f35acbf845825206b8a40
PFManuel <Manuelperez1645@outlook.com>
2024-07-29 18:57:26 -0600

    Primer compromiso

---
+++ src/main/java/org/esfe/SysControlDeEntradaYSalidaDePersonalApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysControlDeEntradaYSalidaDePersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysControlDeEntradaYSalidaDePersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/SysControlDeEntradaYSalidaDePersonal/SysControlDeEntradaYSalidaDePersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.SysControlDeEntradaYSalidaDePersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysControlDeEntradaYSalidaDePersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
